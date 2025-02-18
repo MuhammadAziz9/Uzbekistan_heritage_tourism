ae4301947f735c91a340d7738ea080a57d1a2129
A1909bek <aa2004bek@gmail.com>
2025-02-18 22:21:40 +0000

    next commit

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import CustomUser
+from .forms import CustomUserChangeForm,CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username','email','nationality','is_staff']
+    fieldsets = UserAdmin.fieldsets + (
+        (None,{'fields':('nationality',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None,{'fields':('nationality',)}),
+    )
+
+admin.site.register(CustomUser,CustomUserAdmin)
